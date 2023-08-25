649d84914964be269c0b29b36462e753e0c746cb
Shahan Arshad <68821506+sazk07@users.noreply.github.com>
2023-08-26 01:11:52 +0500

    Add 404 error handler

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express'
 import path from 'path'
 import cookieParser from 'cookie-parser';
 import logger from 'morgan'
+import createHttpError from 'http-errors';
 
 import apiRoutes from './routes/api.js'
 
@@ -17,5 +18,21 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', apiRoutes);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createHttpError(404))
+})
+
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get("env") === "development" ? err : {}
+
+  // render the error page
+  res.status(err.status || 500)
+  res.render("error")
+})
 
 export { app }
