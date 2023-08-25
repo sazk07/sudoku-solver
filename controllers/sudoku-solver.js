c4074c1ebbbfc47cf75aefea1f8c79d0b0b43afb
Shahan Arshad <68821506+sazk07@users.noreply.github.com>
2023-08-25 15:41:02 +0500

    Add solve functionality

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -22,6 +22,10 @@ class SudokuSolver {
     }
 
     this.#createGridFromString(puzzleString)
+    const isLegal = SudokuSolver.#isDigitLegal({ emptyCellsAllowed: true })
+    if (!isLegal) {
+      throw new Error('Puzzle cannot be solved')
+    }
     return null
   }
 
@@ -79,7 +83,99 @@ class SudokuSolver {
   }
 
   solve(puzzleString) {
+    this.#createGridFromString(puzzleString)
+    if (puzzleString.match(/\./gi)) {
+      this.#recursions = 0
+      return this.#solveSudoku(this.#puzzle)
+    }
+    return this.#isSolutionComplete()
+  }
 
+  #solveSudoku(grid) {
+    this.#recursions++
+    if (this.#recursions > 250000) {
+      return false
+    }
+    // check if all slots are filled
+    const [row, col] = SudokuSolver.#getUnassignedLocation(grid)
+    if (row===10 || col===10) {
+      return true
+    }
+    // try 1 to 9 for each zero present in the grid
+    for (let value = 1; value < 10; value++) {
+      if (SudokuSolver.#isSafe(grid, row, col, value)) {
+        grid[row][col] = value
+        if (this.#solveSudoku(grid)) {
+          return true
+        }
+        grid[row][col] = 0
+      }
+    }
+    return false
+  }
+
+  static #getUnassignedLocation(grid) {
+   for (let row = 0; row < TOTALHEIGHT; row++) {
+      for (let col = 0; col < TOTALWIDTH; col++) {
+        if (grid[row][col]===0) {
+          return [row, col]
+        }
+      }
+    }
+    return [10, 10]
+  }
+
+  static clearTargetCellInPuzzle(puzzleString, row, col) {
+    this.#createGridFromString(puzzleString)
+    this.#puzzle[row][col] = 0
+    return SudokuSolver.#createStringFromGrid()
+  }
+
+  static #createStringFromGrid() {
+    let output = ''
+    for (let row = 0; row < TOTALHEIGHT; row++) {
+      for (let col = 0; col < TOTALWIDTH; col++) {
+        output += this.#puzzle[row][col] ? this.#puzzle[row][col].toString() : '.'
+      }
+    }
+    return output
+  }
+
+  static invalidCoordinateError() {
+    return new Error('Invalid coordinate')
+  }
+
+  getCoordinate(coord) {
+    coord = coord.toUpperCase()
+    const [letterPortion, numPortion] = [...coord]
+    const valNumPortion = parseInt(numPortion)
+    // coordinate value should be string with len 2
+    if (typeof coord !== 'string' || coord.length !== 2) {
+      throw SudokuSolver.invalidCoordinateError()
+    }
+    // check for coord row matches A to I
+    if (letterPortion.match(/[^A-I]/)) {
+      throw SudokuSolver.invalidCoordinateError()
+    }
+    const row = letterPortion.codePointAt(0) - 'A'.codePointAt(0)
+    // second digit must be a number
+    if (!valNumPortion) {
+      throw SudokuSolver.invalidCoordinateError()
+    }
+    const isBetween = (num, min, max) => {
+      return min < num && num < max
+    }
+    if (!isBetween(valNumPortion, 0, 10)) {
+      throw SudokuSolver.invalidCoordinateError()
+    }
+    const col = valNumPortion - 1
+    return [row, col]
+  }
+
+  #isSolutionComplete() {
+    return SudokuSolver.#isDigitLegal({
+      emptyCellsAllowed: false
+    })
   }
 
   static #isDigitLegal({ emptyCellsAllowed }) {
@@ -116,6 +212,9 @@ class SudokuSolver {
 
   static #isSafe(grid, row, col, value) {
     // check is num placed in row, col or subgrid?
+    if (SudokuSolver.#isUsedInRow(grid, row, value) || SudokuSolver.#isUsedInColumn(grid, col, value) || SudokuSolver.#isUsedInSubGrid(grid, row-(row%3), col-(col%3), value)) {
+      return false
+    }
     return true
   }
   static #isValidInput(inputChar) {
