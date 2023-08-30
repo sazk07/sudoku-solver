bce2f05ed9cbcaaba5ded0fced6d4c5fed12993a
Shahan Arshad <68821506+sazk07@users.noreply.github.com>
2023-08-31 00:41:27 +0500

    remove tests and console log calls

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@ import { Router } from 'express';
 const router = Router()
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log(process.cwd())
   res.sendFile(process.cwd() + '../public/index.html')
 });
 
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,4 +1,4 @@
-import { suite, suiteSetup, test } from "mocha";
+import { suite, test } from "mocha";
 import { assert } from "chai";
 import { Solver } from '../controllers/sudoku-solver.js'
 const inputStr = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..'
